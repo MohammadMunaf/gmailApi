fb475e62b9e045baaeb4f7f1913fa1f2c18303ff
Mohammad Munaf <mohammadmehfooz100@gmail.com>
2023-12-18 19:24:40 +0530

    create login page and connect port

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express=require('express');
+const PORT = process.env.PORT || 3000;
 const app=express();
 
 app.set('view engine','ejs');
 const path=require('path')
 app.set('views',path.join(__dirname,'/views'));
+
+app.get('/',(req,res)=>{
+    res.render('login');
+})
+app.post('/',(req,res)=>{
+
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Listining on port ${PORT}`)
+})
